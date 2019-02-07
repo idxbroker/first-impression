9b43643613739e9dc63d198204cbfdfcfb7454ec
Erik Brown <erik@idxbroker.com>
2019-02-07 11:08:26 -0800

    Fixed font issues and enqueued foundation for listing details table styling

--- single-listing-elegant.php
+++ single-listing-elegant.php
@@ -30,6 +30,12 @@ function dequeue_first_impression() {
 }
 add_action( 'wp_print_styles', 'dequeue_first_impression', 100 );
 
+function enqueue_foundation() {
+	wp_enqueue_style( 'equity-foundation', PARENT_URL . '/lib/css/foundation.css', false, null );
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_foundation' );
+
+
 // Remove First Impression contact section
 remove_action( 'equity_before_footer', 'must_see_before_footer', 1 );
 
@@ -207,7 +213,7 @@ function single_listing_style() { ?>
 	body.listing-template-custom h6 {
 		font-size: 20px;
 	}
-	.entry-content p {
+	body .entry-content p {
 		font-family: "Raleway", "Open Sans", sans-serif;
 		font-size: 18px;
 		line-height: 30px;
